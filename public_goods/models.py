9e1160135e026b2055f8df8b2418b037b81cc6ee
Frueh <seraina.frueh@unisg.ch>
2018-06-07 16:03:42 +0200

    'game_intro_otree_ACA'

---
+++ public_goods/models.py
@@ -0,0 +1,41 @@
+from otree.api import (
+    models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
+    Currency as c, currency_range
+)
+
+
+author = 'smfrueh'
+
+doc = """
+Your app description
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'public_goods'
+    players_per_group = 3
+    num_rounds = 3
+    endowment = 100
+    coefficient = 2
+    path_to_instructions = 'public_goods/InstructionsBLOCK.html'
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pg_group_contr = models.IntegerField()
+    pg_average_contr = models.IntegerField()
+    def set_payoffs(self):
+        self.pg_group_contr = sum([p.pg_ind_contr for p in self.get_players()])
+        self.pg_average_contr = self.pg_group_contr / Constants.players_per_group
+        split_pg_group_contr = (self.pg_group_contr * Constants.coefficient) / Constants.players_per_group
+        for p in self.get_players():
+            p.payoff = Constants.endowment - p.pg_ind_contr + split_pg_group_contr
+
+
+class Player(BasePlayer):
+    pg_ind_contr = models.CurrencyField(min=0,
+                                        max=Constants.endowment,
+                                        verbose_name='How much do you want to contribute to the project?', )
---
+++ public_goods/pages.py
@@ -0,0 +1,43 @@
+from otree.api import Currency as c, currency_range
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+
+class Intro(Page):
+    def is_displayed(self):
+        return self.round_number == 1
+
+
+class Contribution(Page):
+    form_model = 'player'
+    form_fields = ['pg_ind_contr']
+
+
+class ResultsWaitPage(WaitPage):
+    def after_all_players_arrive(self):
+        self.group.set_payoffs()
+
+
+class Results(Page):
+    def vars_for_template(self):
+        data = [g.pg_average_contr for g in self.group.in_all_rounds()]
+        series = [{'name': 'Average Contribution', 'type': 'column', 'data': data}]
+        return {'series': series}
+
+
+class End(Page):
+    def vars_for_template(self):
+        image1 = 'public_goods_game/smiley2.png'
+        return {'image1': image1}
+
+    def is_displayed(self):
+        return self.round_number == Constants.num_rounds
+
+
+page_sequence = [
+    Intro,
+    Contribution,
+    ResultsWaitPage,
+    Results,
+    End,
+]
